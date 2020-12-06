065fc6fe9ee93fde84746a9087548ff95a17e9f2
trickypr <trickypr@users.noreply.github.com>
2020-12-06 13:29:01 +1100

    Remove rust

--- ui/level_creator/LevelCreator.gd
+++ ui/level_creator/LevelCreator.gd
@@ -8,7 +8,7 @@ func _create_world():
 	var root = get_node("/root")
 	
 	var next_level = world.instance()
-	next_level.world_size = world_size.value
+	next_level.world_size = int(world_size.value)
 	root.add_child(next_level)
 	
 	root.remove_child(self)
